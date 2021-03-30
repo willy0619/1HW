05bf9831ed626710ea18c375774d704b74a20ffd
willy0619 <74764061+willy0619@users.noreply.github.com>
2021-03-30 13:01:42 +0800

    Update Default.aspx.cs

--- 1HW/Default.aspx.cs
+++ 1HW/Default.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,7 +8,23 @@ using System.Web.UI.WebControls;
 namespace _1HW {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-
+            int n = 12;
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    sum += 1;
+                }
+            }
+            if (sum == 2)
+            {
+                Response.Write("YES");
+            }
+            else
+            {
+                Response.Write("NO");
+            }
         }
     }
 }
